0d52083716dc38217f94586e1f1f1b171d38c262
Vishal Sharma <vs8037120@gmail.com>
2024-10-24 21:03:47 +0530

    manage expense

---
+++ src/dummyTransactions.js
@@ -0,0 +1,15 @@
+
+import foodIcon from "./assets/food.svg";
+import movieIcon from "./assets/movie.svg";
+import travelIcon from "./assets/travel.svg";
+
+export const dummyData = [
+    {
+      name: "Samosa",
+      date: "2024-03-18",
+      category: "food",
+      price: 0,
+      id: 1711460238574
+    }
+  ]
+
